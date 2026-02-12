2aa6cabf19944ed0e40171f70fb58ffae54a86a9
Dima <dima.dmitry.kulikov@mail.ru>
2026-02-12 18:09:54 +0300

    Сode cleaning

---
+++ app/components/AppHeaderLogo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
+
+<template>
+  <NuxtImg
+    v-if="appConfig.header?.logo?.light"
+    :src="appConfig.header.logo.light"
+    :alt="appConfig.header.logo.alt || appConfig.header?.title || 'Logo'"
+    v-bind="$attrs"
+  />
+  <span v-else>{{ appConfig.header?.title || 'FeuerSoftware' }}</span>
+</template>
